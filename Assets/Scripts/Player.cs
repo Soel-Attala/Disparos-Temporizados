c4dda3fe96a93514e253f83741df95ee533cd2c8
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2022-07-01 11:50:43 -0300

    creamos metodos y variables para el movimiento

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float hor;
+    public float ver;
+    public float speed;
     void Start()
     {
         
@@ -13,6 +15,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovimientoJugador();
+    }
+
+    void MovimientoJugador()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hor, 0, ver) * speed * Time.deltaTime);
+
+
     }
 }
